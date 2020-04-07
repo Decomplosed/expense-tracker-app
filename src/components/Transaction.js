bdb60a4c03af3e81d2b7a55cb052bee3df63aff9
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-07 10:19:20 +0200

    added dynamic classname for li element and fixed classname of delete button

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,13 +4,13 @@ const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+'
 
   return (
-    <li className=''>
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text}
       Cash{' '}
       <span>
         {sign}${Math.abs(transaction.amount)}
       </span>
-      <button className='delete-button'>x</button>
+      <button className='delete-btn'>x</button>
     </li>
   )
 }
