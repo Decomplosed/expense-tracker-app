514e544f2231d33225a56e022fc1b5f24da7a226
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-07 09:49:45 +0200

    created basic TransactionList

---
+++ src/components/TransactionList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TransactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul id='list' className='list'></ul>
+    </>
+  )
+}
+
+export default TransactionList
