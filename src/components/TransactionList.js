11d061ec37e5824dcf5fa8741acfc264bc35df26
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-07 10:11:37 +0200

    started creating list items for transaction lsit based on global state

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,10 +2,20 @@ import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext)
+
   return (
     <>
       <h3>History</h3>
-      <ul id='list' className='list'></ul>
+      <ul id='list' className='list'>
+        {transactions.map((transaction) => (
+          <li className=''>
+            {transaction.amount}
+            Cash <span></span>
+            <button className='delete-button'>x</button>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
