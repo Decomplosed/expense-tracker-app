a8a2032639e7276faed7a3484e726875a36c262c
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-08 08:28:03 +0200

    started implementing env variables

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
+
+dotenv.config({ path: './config/config.env' })
+
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello'))
+
+app.listen()
