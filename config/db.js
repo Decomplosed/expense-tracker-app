685cbf90c457202c5d1d4729bc4b827089c98d91
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-08 09:01:33 +0200

    setup connection inside try block

--- config/db.js
+++ config/db.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
-  try {} catch(err) {}
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    })
+  } catch (err) {}
 }
