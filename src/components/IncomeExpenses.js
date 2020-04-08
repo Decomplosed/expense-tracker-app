b7949fbbfc5c00b36feb9ad8942e21e3a2483756
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-08 07:45:02 +0200

    created expense variable and implemented dynamic income and expense to the template

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -11,18 +11,23 @@ const IncomeExpenses = () => {
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2)
 
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2)
+
   return (
     <div className='inc-exp-container'>
       <div>
         <h4>Income</h4>
         <p id='money-plus' className='money plus'>
-          +$0.00
+          {income}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id='money-minus' className='money minus'>
-          -$0.00
+          {expense}
         </p>
       </div>
     </div>
