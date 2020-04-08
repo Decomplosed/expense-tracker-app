a3c1bc2a3d81369a501cbb02a688fc7cf2d3e699
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-08 08:00:22 +0200

    created addTransaction method

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -22,6 +22,13 @@ export const GlobalProvider = ({ children }) => {
     })
   }
 
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    })
+  }
+
   return (
     <GlobalContext.Provider
       value={{
