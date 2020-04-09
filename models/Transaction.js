5baf4f9489dd6e86f9456475c2ace36fdb412c69
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-09 07:34:27 +0200

    created TransactionSchema

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,3 +1,17 @@
 const mongoose = require('mongoose')
 
-const TransactionSchema = new mongoose.Schema({})
+const TransactionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add some text'],
+  },
+  amount: {
+    type: Number,
+    required: [true, 'Please add a postivie or negative number']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
