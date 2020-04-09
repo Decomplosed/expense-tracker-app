d6045f1158697fe870edac0bef4f38dbc8a2db03
Bart Frackiewicz <decomplosed@gmail.com>
2020-04-09 09:15:42 +0200

    extracted getTransactions from Context

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -3,7 +3,7 @@ import { GlobalContext } from '../context/GlobalState'
 import Transaction from './Transaction'
 
 const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext)
+  const { transactions, getTransactions } = useContext(GlobalContext)
 
   return (
     <>
